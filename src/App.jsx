e5ec162b88d7e7894001f5e8e681fc7f7e3f4f5a
siddiqfathurahman <fr9299540@gmail.com>
2024-11-05 19:55:29 +0700

    create tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Hello, Tailwind CSS!</h1>
+      <p className="text-lg text-gray-700">
+        Jika Anda melihat gaya ini, berarti Tailwind CSS sudah berfungsi dengan benar.
       </p>
+      <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+        Klik Saya
+      </button>
+    </div>
     </>
   )
 }

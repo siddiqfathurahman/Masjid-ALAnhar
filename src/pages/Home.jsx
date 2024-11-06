1f780fea187a7af383299af648f21605cd3088fb
siddiqfathurahman <fr9299540@gmail.com>
2024-11-06 21:06:18 +0700

    create font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen font-poppins">
       <h1 className="text-2xl font-bold">Ini merupakan halaman home</h1>
     </div>
   );

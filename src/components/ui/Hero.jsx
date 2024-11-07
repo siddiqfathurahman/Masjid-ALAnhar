98a1785fadbe2dda58c9672a91d47f7af618d114
siddiqfathurahman <fr9299540@gmail.com>
2024-11-07 19:57:14 +0700

    add box api

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,15 +1,18 @@
 const Hero = () => {
-    return (
-      <div className="w-full h-[400px]">
-        <img 
-          className="w-full h-full object-cover" 
-          style={{ objectPosition: 'center 70%' }} 
-          src="/masjid.jpg" 
-          alt="Masjid"
-        />
-      </div>
-    );
-  };
-  
-  export default Hero;
-  
+  return (
+    <div className="relative w-full h-[400px]">
+      <img 
+        className="w-full h-full object-cover" 
+        style={{ objectPosition: 'center 70%' }} 
+        src="/masjid.jpg" 
+        alt="Masjid"
+      />
+      <div
+        className="absolute left-1/2 -translate-x-1/2 w-[70%] h-[50%] bg-green-800 rounded-2xl"
+        style={{ bottom: '-25%' }}
+      ></div>
+    </div>
+  );
+};
+
+export default Hero;

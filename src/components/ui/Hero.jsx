0a4d2d157e043de830bc77c3837c49c4e9adaae6
siddiqfathurahman <fr9299540@gmail.com>
2024-11-09 21:07:10 +0700

    update responsive

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -25,8 +25,8 @@ const Hero = () => {
   }, []);
 
   return (
-    <div>
-      <div className="relative w-full h-[400px]">
+    <div className=" lg:px-0">
+      <div className="relative w-full h-[250px] md:h-[350px] lg:h-[400px]">
         <img 
           className="w-full h-full object-cover" 
           style={{ objectPosition: 'center 70%' }} 
@@ -34,27 +34,27 @@ const Hero = () => {
           alt="Masjid"
         />
         <div
-          className="absolute left-1/2 -translate-x-1/2 w-[70%] h-[50%] bg-green-800 rounded-2xl"
-          style={{ bottom: '-25%' }}
+          className="absolute left-1/2 -translate-x-1/2 w-[90%] md:w-[80%] lg:w-[70%] h-[40%] md:h-[50%] bg-green-800 rounded-2xl"
+          style={{ bottom: '-20%' }}
         ></div>
       </div>
 
-     
-      <div className="font-poppins text-center mt-36">
-        <h1 className="text-5xl font-extrabold">
-          <span className="text-black">Profil </span>
-          <span 
-            ref={titleRef} 
-            className={`relative inline-block p-1 text-white ${isVisible ? 'animate-gradient-box' : ''}`}
-          >
-            MASJID AL-ANHAR
-          </span>
-        </h1>
-        <p className="text-xl max-w-4xl mt-3 mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi neque esse cum fuga, 
-          repudiandae repellat obcaecati quidem? Id ab iusto quod a illo eius itaque cupiditate beatae nisi aspernatur?
-        </p>
+      <div className=" md:px-0 font-poppins mt-20 md:mt-28 lg:mt-36 text-left md:text-center">
+        <h1 className="px-5 text-3xl md:text-4xl lg:text-5xl font-extrabold">
+        <span className="text-black">Profil </span>
+        <span 
+        ref={titleRef} 
+        className={`relative inline-block p-1 text-white ${isVisible ? 'animate-gradient-box' : ''}`}
+        >
+        MASJID AL-ANHAR
+      </span>
+      </h1>
+      <p className=" px-5 text-lg md:text-xl max-w-2xl md:max-w-3xl lg:max-w-4xl mt-2 md:mt-3 mx-auto text-left md:text-center px-3 md:px-0">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eligendi neque esse cum fuga, 
+        repudiandae repellat obcaecati quidem? Id ab iusto quod a illo eius itaque cupiditate beatae nisi aspernatur?
+      </p>
       </div>
+
     </div>
   );
 };

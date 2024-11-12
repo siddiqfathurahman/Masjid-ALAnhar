e1ce416d8e333c7041c144024bc9a957811016a3
siddiqfathurahman <fr9299540@gmail.com>
2024-11-12 18:15:36 +0700

    add logokonten

---
+++ src/components/ui/Logokonten.jsx
@@ -0,0 +1,15 @@
+const Logo = () => {
+    return (
+        <div>
+                <h1 className="font-poppins text-3xl font-extrabold text-center">Organisasi</h1>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+        </div>
+    );
+};
+
+export default Logo;

213ea0d1718ece7ef760a3c5597cd4e17ea31140
siddiqfathurahman <fr9299540@gmail.com>
2024-11-16 21:30:51 +0700

    add program

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -25,7 +25,7 @@ const Program = () => {
   }, []);
 
   return (
-    <div className='font-poppins text-center flex flex-col items-center mb-10 mt-10'>
+    <div className='font-poppins text-center flex flex-col items-center mb-10 mt-10 px-5'>
       <h1 className='text-3xl font-extrabold'>Program Dakwah</h1>
       <h2 className='text-3xl md:text-4xl lg:text-5xl font-extrabold'>
         <span
@@ -35,16 +35,16 @@ const Program = () => {
           MASJID AL-ANHAR
         </span>
       </h2>
-      <div className='flex gap-32 mt-12'>
+      <div className='flex flex-col md:flex-row gap-10 md:gap-32 mt-12 items-center'>
         <div className='flex flex-col items-center'>
-          <img src="/bg.JPG" alt="pengajian" className='w-96 h-96 rounded-2xl' />
-          <p className='max-w-xs text-left'>
+          <img src="/bg.JPG" alt="pengajian" className='w-full max-w-md h-auto rounded-2xl' />
+          <p className='md:max-w-md pt-3text-left'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, alias sint. Magnam eveniet quas dolorum dolor consectetur a officia aspernatur. Minima similique, perferendis corrupti nulla consequuntur repudiandae dicta necessitatibus impedit.
           </p>
         </div>
         <div className='flex flex-col items-center'>
-          <img src="/bg.JPG" alt="pengajian" className='w-96 h-96 rounded-2xl' />
-          <p className='max-w-xs text-left'>
+          <img src="/bg.JPG" alt="pengajian" className='w-full max-w-md h-auto rounded-2xl' />
+          <p className='md:max-w-md pt-3 text-left'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quibusdam reprehenderit eligendi, maiores ad officiis ipsa tenetur vitae laudantium iste ut quam voluptatem exercitationem et at labore ducimus porro itaque.
           </p>
         </div>

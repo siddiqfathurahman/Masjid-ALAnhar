0bc52c2851ba1017cd59aeecb70b83028485414e
Muhammad Siddiq Fathurahman <129701382+siddiqfathurahman@users.noreply.github.com>
2024-11-19 21:43:04 +0700

    Update Tpa.jsx

--- src/pages/Tpa.jsx
+++ src/pages/Tpa.jsx
@@ -1,8 +1,6 @@
 const Tpa = () => {
     return (
-        <div className="flex items-center justify-center h-screen font-poppins">
-            <h1 className="text-2xl font-bold">Ini merupakan halaman tpa</h1>
-        </div>
+
     )
 }
 

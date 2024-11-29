96f4c5ecfc4706ad1f63acb3dce9e8d55d8bef98
siddiqfathurahman <fr9299540@gmail.com>
2024-11-29 20:40:38 +0700

    update style layanan

--- src/components/ui/Layanan.jsx
+++ src/components/ui/Layanan.jsx
@@ -1,11 +1,10 @@
 const Layanan = () => {
   const layananData = [
     { id: 1, title: "Fasilitas air minum gratis", image: "/dispenser.jpeg" },
-    { id: 2, title: "Tempat ibadah nyaman dengan pendingin ruangan (AC)", image: "/ac.jpeg" },
+    { id: 2, title: "Tempat ibadah nyaman dengan pendingin ruangan", image: "/ac.jpeg" },
     { id: 3, title: "Tempat wudhu bersih dan mudah diakses", image: "/dispenser.jpeg" },
     { id: 4, title: "Tersedia Al-Qur'an untuk mendukung kegiatan ibadah", image: "/ac.jpeg" },
   ];
-  
 
   return (
     <div className="text-center mt-20 font-poppins mb-10 px-4 md:px-0">
@@ -16,15 +15,14 @@ const Layanan = () => {
         Masjid Al-Anhar menyediakan berbagai layanan untuk mendukung kebutuhan ibadah dan kegiatan sosial umat. Kami berkomitmen untuk menciptakan lingkungan yang nyaman dan aman bagi seluruh jamaah.
       </p>
 
-    
       <div className="grid md:px-40 grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10 justify-items-center">
         {layananData.map((layanan) => (
-          <div key={layanan.id} className="relative">
-            <div className="md:w-[200px] md:h-[250px] w-[175px] h-[250px]  bg-gray-300 rounded-lg overflow-hidden">
+          <div key={layanan.id} className="relative group">
+            <div className="md:w-[200px] md:h-[250px] w-[170px] h-[250px] bg-gray-300 rounded-lg overflow-hidden">
               <img
                 src={layanan.image}
                 alt={layanan.title}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
               />
               <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-t opacity-80 from-black to-transparent px-3 pt-44 rounded-b-lg">
                 <h2 className="text-white text-sm leading-snug text-left">{layanan.title}</h2>

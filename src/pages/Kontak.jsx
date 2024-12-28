262b84422ee59521147b762bd15ba723252c1e6d
siddiqfathurahman <fr9299540@gmail.com>
2024-12-28 19:28:40 +0700

    update kontak

--- src/pages/Kontak.jsx
+++ src/pages/Kontak.jsx
@@ -7,7 +7,7 @@ const Kontak = () => {
     <div className="font-poppins mb-10">
       <div className="md:mt-10 md:ml-10 mt-10 ml-3">
         <h1 className="font-extrabold text-3xl">Kontak</h1>
-        <p>
+        <p className="md:max-w-[600px]">
           Kami siap membantu Anda! Jika Anda memiliki pertanyaan atau ingin
           menghubungi kami, Anda dapat memilih salah satu cara berikut:
         </p>
@@ -62,7 +62,7 @@ const Kontak = () => {
           className="bg-yellow-400 flex items-center space-x-2 text-2xl font-bold text-white rounded-lg text-center p-2 mb-4 md:mb-0"
         >
           <MdEmail />
-          <span>ramahofficialyk@gmail.com</span>
+          <span>alanharkeparakankidul</span>
         </a>
 
         <a

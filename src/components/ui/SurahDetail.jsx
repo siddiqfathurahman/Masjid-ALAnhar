cdcb1793b753e72e870d02bff26a046b57cf01ab
siddiqfathurahman <fr9299540@gmail.com>
2025-01-21 19:34:24 +0700

    update menu pencarian surat

--- src/components/ui/SurahDetail.jsx
+++ src/components/ui/SurahDetail.jsx
@@ -2,16 +2,20 @@ import { useState, useEffect } from 'react';
 import { IoIosArrowBack, IoIosSettings } from "react-icons/io";
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import SettingsModal from './SettingsModal'; 
+import SettingsModal from './SettingsModal';
+import { IoSearch } from "react-icons/io5";
 
 function SurahDetail() {
   const { id } = useParams();
   const [surah, setSurah] = useState(null);
   const [loading, setLoading] = useState(true);
   const [modalOpen, setModalOpen] = useState(false);
-  const [arabicSize, setArabicSize] = useState(24);  
-  const [latinSize, setLatinSize] = useState(16);    
-  const [showLatin, setShowLatin] = useState(true);  
+  const [searchModalOpen, setSearchModalOpen] = useState(false); // Modal pencarian
+  const [searchQuery, setSearchQuery] = useState(""); // Input pencarian
+  const [filteredVerses, setFilteredVerses] = useState([]); // Hasil filter
+  const [arabicSize, setArabicSize] = useState(24);
+  const [latinSize, setLatinSize] = useState(16);
+  const [showLatin, setShowLatin] = useState(true);
   const [showTranslation, setShowTranslation] = useState(true);
   const navigate = useNavigate();
 
@@ -20,6 +24,7 @@ function SurahDetail() {
       try {
         const response = await axios.get(`https://api.quran.gading.dev/surah/${id}`);
         setSurah(response.data.data);
+        setFilteredVerses(response.data.data.verses); 
         setLoading(false);
       } catch (error) {
         console.error('Error fetching surah detail:', error);
@@ -27,6 +32,19 @@ function SurahDetail() {
     };
     getSurahDetail();
   }, [id]);
+
+  const handleGoBack = () => {
+    navigate('/al-quran');
+  };
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const toggleSearchModal = () => {
+    setSearchModalOpen(!searchModalOpen);
+    setSearchQuery(""); 
+  };
 
   const convertToArabicNumerals = (number) => {
     const arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
@@ -36,14 +54,21 @@ function SurahDetail() {
       .map((digit) => arabicDigits[digit])
       .join('');
   };
-  
 
-  const handleGoBack = () => {
-    navigate('/al-quran');
-  };
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
 
-  const toggleModal = () => {
-    setModalOpen(!modalOpen);
+    if (query === "" || !surah) {
+      setFilteredVerses(surah.verses); 
+    } else {
+      const index = parseInt(query) - 1;
+      if (index >= 0 && index < surah.verses.length) {
+        const selectedVerse = surah.verses[index];
+        const remainingVerses = surah.verses.filter((_, i) => i !== index);
+        setFilteredVerses([selectedVerse, ...remainingVerses]);
+      }
+    }
   };
 
   if (loading) {
@@ -59,20 +84,22 @@ function SurahDetail() {
       <div className="w-full max-w-3xl">
         <div className="flex-col w-full p-5 rounded-md shadow-lg bg-white text-black">
           <div className="flex items-center justify-between gap-2">
-            <IoIosArrowBack 
-              className="md:text-2xl mb-2 text-2xl cursor-pointer" 
-              onClick={handleGoBack} 
+            <IoIosArrowBack
+              className="md:text-2xl mb-2 text-2xl cursor-pointer"
+              onClick={handleGoBack}
             />
-            <div className="">
-              <h1 className="md:text-4xl text-2xl font-semibold">{surah.number}. {surah.name.transliteration.id}</h1>
-              <p className="md:text-sm text-center text-xs md:text-sm">{surah.name.translation.id} | {surah.numberOfVerses} Ayat</p>
+            <div className="text-center">
+              <h1 className="md:text-4xl text-1xl font-semibold">{surah.number}. {surah.name.transliteration.id}</h1>
+              <p className="md:text-sm text-xs">{surah.name.translation.id} | {surah.numberOfVerses} Ayat</p>
             </div>
-            <IoIosSettings className='md:text-3xl text-2xl cursor-pointer' onClick={toggleModal} />
+            <div className='flex gap-3 items-center'>
+            <IoSearch className="text-2xl cursor-pointer" onClick={toggleSearchModal} />
+            <IoIosSettings className="md:text-3xl text-2xl cursor-pointer" onClick={toggleModal} />
+            </div>
           </div>
           <hr className="opacity-90 mt-2" />
           <h1 className="text-3xl text-center font-amiri justify-center mb-10 mt-14">بِسْــــــــمِ اللهِ الرَّحْمَنِ الرَّحِيْمِ</h1>
 
-          
           <SettingsModal
             modalOpen={modalOpen}
             toggleModal={toggleModal}
@@ -86,38 +113,77 @@ function SurahDetail() {
             setShowTranslation={setShowTranslation}
           />
 
-          <div className="mt-5">
-            {surah.verses.map((verse, i) => (
-              <div key={i} className="mb-4">
-                <div className="flex justify-between mb-4 items-center">
-                  <p
-                    className="relative text-white  text-sm  w-14 h-14 bg-no-repeat bg-center bg-cover flex items-center justify-center"
-                    style={{
-                      backgroundImage: `url('/ayat.png')`,
-                    }}
-                  >
-                    {i + 1}
-                  </p>
-                </div>
-                <p className="text-right font-amiri leading-[60px] mb-3" style={{ fontSize: `${arabicSize}px` }}>
-                  {verse.text.arab} 
-                  
-                  <span className="" > ({convertToArabicNumerals(i + 1)})</span>
-                </p>
-                {showLatin && (
-                  <p className="text-left text-hijau" style={{ fontSize: `${latinSize}px` }}>
-                    {verse.text.transliteration.en}
-                  </p>
-                )}
-                {showTranslation && (
-                  <p className="text-left mt-2 opacity-80">
-                    {verse.translation.id}
-                  </p>
-                )}
-                <hr className="my-3 opacity-50" />
-              </div>
-            ))}
+          {/* Modal Pencarian */}
+          {searchModalOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-20 flex justify-center items-center z-[9999]">
+            <div className="bg-white p-5 rounded-lg text-center shadow-lg max-w-md w-60">
+              <h2 className="text-lg font-semibold mb-3">Cari Nomor Ayat</h2>
+                <input
+                type="number"
+                className="border text-center border-gray-300 rounded px-3 py-2 w-full mb-3 appearance-none no-spin"
+                placeholder="nomor ayat"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                style={{
+                  WebkitAppearance: 'none',
+                  MozAppearance: 'textfield',
+                }}
+                />
+              <button
+                onClick={toggleSearchModal}
+                className="bg-hijau text-xs font-bold text-white px-4 py-2 rounded"
+              >
+              Tutup
+              </button>
+            </div>
           </div>
+        )}
+
+
+<div className="mt-5">
+  {surah.verses.map((verse, i) => (
+    <div
+      key={verse.number.inSurah}
+      ref={(el) => {
+        if (parseInt(searchQuery, 10) === verse.number.inSurah) {
+          el?.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+      }}
+      className="mb-4"
+    >
+      <div className="flex justify-between mb-4 items-center">
+        <p
+          className="relative text-white text-sm w-14 h-14 bg-no-repeat bg-center bg-cover flex items-center justify-center"
+          style={{
+            backgroundImage: `url('/ayat.png')`,
+          }}
+        >
+          {verse.number.inSurah}
+        </p>
+      </div>
+      <p
+        className="text-right font-amiri leading-[60px] mb-3"
+        style={{ fontSize: `${arabicSize}px` }}
+      >
+        {verse.text.arab}
+        <span className="" > ({convertToArabicNumerals(i + 1)})</span>
+      </p>
+      {showLatin && (
+        <p
+          className="text-left text-hijau"
+          style={{ fontSize: `${latinSize}px` }}
+        >
+          {verse.text.transliteration.en}
+        </p>
+      )}
+      {showTranslation && (
+        <p className="text-left mt-2 opacity-80">{verse.translation.id}</p>
+      )}
+      <hr className="my-3 opacity-50 " />
+    </div>
+  ))}
+</div>
+
         </div>
       </div>
     </div>

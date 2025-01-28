03d35dd0100acc069e5df59e2d96e170cbaef4aa
siddiqfathurahman <fr9299540@gmail.com>
2025-01-27 16:44:34 -0800

    update text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom"; 
 
 const Footer = () => {
@@ -76,10 +75,10 @@ const Footer = () => {
               ))}
             </ul>
             <div className="pt-5">
-              <h1 className="text-white font-bold mb-2 ">Qur'an Digital</h1>
+              <h1 className="text-white font-bold mb-2 ">Quran Digital</h1>
               <Link to="/al-quran">
               <button className="bg-lemon text-hijau py-1 px-2 rounded">
-              Al-Qur'an
+              Al-Quran
               </button>
               </Link>
             </div>
@@ -98,7 +97,7 @@ const Footer = () => {
             <div className="w-full flex justify-center sm:justify-start">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.7714640029835!2d110.37169497412104!3d-7.813999377588248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a579ed87d4a77%3A0x75276a25a1d74088!2sMasjid%20Al%20Anhar!5e0!3m2!1sid!2sid!4v1731558857405!5m2!1sid!2sid"
-                width="300"
+                width="280"
                 height="200"
                 style={{ border: 0 }}
                 allowFullScreen=""
